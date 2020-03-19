d64c78f28c80064ed8daec49a97d4772c719ce14
sadliver <guideji_stitchnoi@hotmail.com>
2020-03-20 05:24:08 +0700

    Ex6 Array+method

---
+++ Ex6_Arrays.js
@@ -0,0 +1,26 @@
+// // Ex1 รูปแบบ
+// let colors = new Array ('red', 'green', 'blue');
+// console.log(colors);
+
+// Ex2.1 เพิ่มค่า(ไม่เป็นที่นิยม)
+// let colors = new Array ('red', 'green', 'blue');
+// colors[3] = 'black';
+// console.log(colors);
+
+// Ex2.2
+// let colors = new Array ('red', 'green', 'blue');
+// colors.push('orange'); //method push จะเป็นการเพิ่มค่าเข้าไปใน Array
+// console.log(colors);
+
+// // Ex 3.1 Method sort number
+let number = [5,11,40,1,90];
+console.log(number.sort());  //Method sort จะเป็นการเรียงค่าค่าจากน้อยไปมาก
+
+
+// Ex 3.2 Method sort string
+let name = ['guide','art','zoom','Art','Guide'];
+console.log(name.sort());  //Method sort จะเป็นการเรียงค่าจากน้อยไปมาก
+
+// Ex 4 Method reverse
+let number1 = [2,5,4,1,10];
+console.log(number1.reverse()); //Method reverse จะเรียงจากมากไปน้อย

cc77cb969fe452a33b8d058e09b064d81f398fed
sadliver <guideji_stitchnoi@hotmail.com>
2020-03-20 04:59:30 +0700

    Ex5 Variables

---
+++ Ex5_Variables.js
@@ -0,0 +1,22 @@
+//Ex1 รูปแบบการเขียน
+
+// Camel Case **ส่วนใหญ่ใช้แบบนี้
+let myFullName;
+
+// Partial Case
+let  myFullName;
+
+// Underscore
+let my_full_name;
+
+// Ex2 Case-Sensitive 
+let name , Name;
+name = 'guide';
+Name = 'ji';
+console.log(name);
+console.log(Name);
+
+// Ex3 String
+let post1 = 'hi bro';
+let post2 = "goodbye bro";
+let post3 = `My name is ${name} . and My nickname is ${nickname}`;  //Backtisks สามารถแทรกตัวแปรระหว่าง string ได้

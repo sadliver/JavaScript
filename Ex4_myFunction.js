f56428b2e511f8c97debf3c90cd3016f372b9a05
sadliver <guideji_stitchnoi@hotmail.com>
2020-03-20 04:40:41 +0700

    Ex4 Function

---
+++ Ex4_myFunction.js
@@ -0,0 +1,9 @@
+// Ex1 call a Function
+function play(){                  //Create a Function
+    console.log("Enjoy!!");   
+}
+play() //call a Function
+
+let greeting = "HiBro";
+let a = greeting.length;       //objectName.propertyName
+console.log(a);
